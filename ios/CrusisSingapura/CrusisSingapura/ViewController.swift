edad05c4739fc715df9af6c00d2333e5f03e803f
Nobin Thomas <nobinthomas@Nobins-MacBook-Pro.local>
2017-06-14 20:25:03 +0800

    Adding basic code templates

---
+++ ios/CrusisSingapura/CrusisSingapura/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrusisSingapura
+//
+//  Created by Nobin Thomas on 14/6/17.
+//  Copyright © 2017 Jesus Youth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
